2f0c47c8a3b9679889cf00a3b3169767f523a5bf
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-08-06 20:29:35 -0400

    Small refactor of code

--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -3,16 +3,17 @@ import { shallow } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents";
 
 describe("<NumberOfEvents /> component", () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  })
   test("render text input", () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find(".number")).toHaveLength(1);
   });
   test("default value of input is 32", () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find(".number").prop("value")).toBe(32);
   });
   test("state changes to user input", () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     const eventObject = { target: {value: 16}};
     NumberOfEventsWrapper.find(".number").simulate("change", eventObject);
     expect(NumberOfEventsWrapper.state("displayCount")).toBe(16);
