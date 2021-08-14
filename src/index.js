708775e4a621af745d7493e6fd76474b93df3e1a
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-08-14 12:48:22 -0400

    added acceptance and end-to-end testing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('7801268a2fb24b53ae050fe1315e4a15').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +24,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
